019e29ab3143b35ace3c43b317a71fc5ffa8473d
Leon Lam <leonlam34@gmail.com>
2026-02-20 21:56:03 -0600

    fixed cron runner

--- backend/cron_runner.py
+++ backend/cron_runner.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 from pathlib import Path
 from datetime import date, timedelta
 
+from fastapi.routing import serialize_response
+
 load_dotenv(Path("/home/theo/dashboard/backend/.env"))
 
 from main import get_all_tasks, check_date, update_task_status, get_sheets_service
@@ -22,17 +24,33 @@ TASK_COLUMNS={
     "cardio": "E",
 }
 
+def pad_row(row, cols):
+    return row + [""] * (cols - len(row))
+
 def main():
     # check the date, minus one to obtain the previous day, which we want to update
     yesterday = date.today() - timedelta(days=1)
     row = check_date(service, SPREADSHEET_ID, yesterday)
 
+    range_name = f"Dailies!B{row}:E{row}"
+
     # get all tasks, find the ones that are blank
-    tasks = get_all_tasks(service, SPREADSHEET_ID, row)
-    for key, value in tasks.items():
-        col = TASK_COLUMNS.get(key)
-        if value == 'No':
-            print(update_task_status(service, SPREADSHEET_ID, row, col, value))
+    api_call = (
+            service.spreadsheets().
+            values().
+            get(spreadsheetId=SPREADSHEET_ID, range=range_name).
+            execute()
+        )
+
+    results = api_call.get('values',[])
+    
+    results = [pad_row(result, 4) for result in results]
+    row_list = results[0]
+    
+    for index, key in enumerate(TASK_COLUMNS):
+        col = row_list[index]
+        if col == '':
+            update_task_status(service, SPREADSHEET_ID, row, TASK_COLUMNS[key], 'No')
 
 if __name__ == "__main__":
     main()
